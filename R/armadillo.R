efa1e1109167bffa501b68d2433f27eea027bdb7
glyptodontid <kimonopangolin@protonmail.com>
2023-06-04 14:28:40 -0600

    Use IBM Plex Sans

--- R/armadillo.R
+++ R/armadillo.R
@@ -35,31 +35,61 @@
 #' @export
 #'
 #' @examples
-theme_glyptodon <- function (base_family = "Roboto Condensed", base_size = 11.5,
-                             plot_title_family = "Noto Sans", plot_title_size = 21, plot_title_face = "bold",
-                             plot_title_margin = 5, subtitle_family = "Source Sans Pro",
-                             subtitle_size = base_size + 1, subtitle_face = "plain", subtitle_margin = 15,
+theme_glyptodon <- function (base_family = "Roboto Condensed",
+                             base_size = 11.5,
 
-                             strip_text_family = base_family, strip_text_size = 12, strip_text_face = "plain",
+                             plot_title_family = "IBM Plex Sans",
+                             plot_title_size = base_size + 10.5,
+                             plot_title_face = "bold",
+                             plot_title_margin = 5,
+
+                             subtitle_family = "Source Sans Pro",
+                             subtitle_size = base_size + 1,
+                             subtitle_face = "plain",
+                             subtitle_margin = 15,
+
+                             strip_text_family = "Arial",
+                             strip_text_size = base_size + .5,
+                             strip_text_face = "plain",
+                             strip_background_col = "#835C3B",
+                             strip_text_col = "#ffffff",
+
                              caption_family = "Roboto",
-                             caption_size = base_size - 2, caption_face = "italic", caption_margin = 10,
+                             caption_size = base_size - 2,
+                             caption_face = "italic",
+                             caption_margin = 10,
+
                              axis_text_family = "Goldman Sans Condensed",
-                             axis_text_size = base_size-.5, axis_title_family = "Arial",
-                             axis_title_size = 10, axis_title_face = "plain", axis_title_just = "rt",
-                             plot_margin = margin(20, 20, 20, 20), grid_col = "#aaaaaa",
-                             grid = TRUE, axis_col = "#333333", axis = TRUE, ticks = TRUE)
+                             axis_text_size = base_size + 2.5,
+                             axis_title_family = "Arial",
+                             axis_title_size = base_size - .5,
+                             axis_title_face = "plain",
+                             axis_title_just = "rt",
+
+                             plot_margin = margin(20, 20, 20, 20),
+                             plot_background = "#eee8d5",
+                             legend_position = "top",
+
+                             grid_major_col = "#70602d",
+                             grid_minor_col = "#d7c99d",
+                             grid = TRUE,
+
+                             axis_col = "#010101",
+                             axis = TRUE,
+                             ticks = TRUE)
 {
   ret <- ggplot2::theme_minimal(base_family = base_family,
                                 base_size = base_size)
   ret <- ret + theme(legend.background = element_blank())
   ret <- ret + theme(legend.key = element_blank())
   if (inherits(grid, "character") | grid == TRUE) {
-    ret <- ret + theme(panel.grid = element_line(color = grid_col,
+    ret <- ret + theme(panel.grid = element_line(color = grid_major_col,
                                                  size = 0.2))
-    ret <- ret + theme(panel.grid.major = element_line(color = grid_col,
-                                                       size = 0.2))
-    ret <- ret + theme(panel.grid.minor = element_line(color = grid_col,
-                                                       size = 0.15))
+    ret <- ret + theme(panel.grid.major = element_line(color = grid_major_col,
+                                                       size = rel(.25)))
+    ret <- ret + theme(panel.grid.minor = element_line(color = grid_minor_col,
+                                                       linetype = "dotted",
+                                                       size = rel(1.25)))
     if (inherits(grid, "character")) {
       if (regexpr("X", grid)[1] < 0)
         ret <- ret + theme(panel.grid.major.x = element_blank())
@@ -121,24 +151,29 @@ theme_glyptodon <- function (base_family = "Roboto Condensed", base_size = 11.5,
   yj <- switch(tolower(substr(axis_title_just, 2, 2)), b = 0,
                l = 0, m = 0.5, c = 0.5, r = 1, t = 1)
   ret <- ret + theme(axis.text.x = element_text(family = axis_text_family,
-                                                color = "#222222",
-                                                size = axis_title_size+5,
+                                                color = axis_col,
+                                                size = axis_text_size,
                                                 margin = margin(t = 2)))
   ret <- ret + theme(axis.text.y = element_text(family = axis_text_family,
-                                                color = "#222222",
-                                                size = axis_title_size+5,
+                                                color = axis_col,
+                                                size = axis_text_size,
                                                 margin = margin(r = 0)))
-  ret <- ret + theme(axis.title = element_text(size = axis_title_size+1,
+  ret <- ret + theme(axis.title = element_text(size = axis_title_size,
                                                family = axis_title_family))
   ret <- ret + theme(axis.title.x = element_text(hjust = xj,
-                                                 size = axis_title_size+1, family = axis_title_family, face = axis_title_face))
+                                                 size = axis_title_size, family = axis_title_family, face = axis_title_face))
   ret <- ret + theme(axis.title.y = element_text(hjust = yj,
-                                                 size = axis_title_size+1, family = axis_title_family, face = axis_title_face))
+                                                 size = axis_title_size, family = axis_title_family, face = axis_title_face))
   ret <- ret + theme(axis.title.y.right = element_text(hjust = yj,
                                                        size = axis_title_size, angle = 90, family = axis_title_family,
                                                        face = axis_title_face))
-  ret <- ret + theme(strip.text = element_text(hjust = 0, size = strip_text_size,
-                                               face = strip_text_face, family = strip_text_family))
+  ret <- ret + theme(strip.text = element_text(hjust = 0.5,
+                                               color = strip_text_col,
+                                               size = strip_text_size,
+                                               face = strip_text_face,
+                                               family = strip_text_family),
+                     strip.background = element_rect(fill = strip_background_col,
+                                                     colour = NA, linetype = 0))
   ret <- ret + theme(panel.spacing = grid::unit(2, "lines"))
   ret <- ret + theme(plot.title = element_text(hjust = 0, size = plot_title_size,
                                                margin = margin(b = plot_title_margin), family = plot_title_family,
@@ -150,24 +185,22 @@ theme_glyptodon <- function (base_family = "Roboto Condensed", base_size = 11.5,
                                                  size = caption_size, margin = margin(t = caption_margin),
                                                  family = caption_family, face = caption_face))
   ret <- ret + theme(plot.margin = plot_margin)
-  ret <- ret + theme(rect = element_rect(fill = "#93a1a1",
-                                         colour = NA, linetype = 1), text = element_text(colour = "Black"),
+  ret <- ret + theme(rect = element_rect(fill = strip_background_col,
+                                         colour = NA, linetype = 1),
+                     text = element_text(colour = "#010101"),
+                     panel.spacing = unit(0.5, "lines"),
+                     legend.title.align = NULL,
+                     legend.position = legend_position,
+                     legend.direction = NULL,
+                     legend.justification = "center",
 
-                     panel.grid.major = element_line(colour = "#70602d",
-                                                     size = rel(.25)),
-                     panel.grid.minor = element_line(colour = "#d7c99d",
-                                                     linetype = "dotted",
-                                                      size = rel(1.25)),
-                     panel.spacing = unit(0.25, "lines"),
-                     strip.background = element_rect(fill = "#93a1a1",
-                                                     colour = NA, linetype = 0),
-                     #strip.text = element_text(size = rel(1.25)),
-                     legend.title.align = NULL, legend.position = "top",
-                     legend.direction = NULL, legend.justification = "center",
-                     plot.background = element_rect(fill = "#eee8d5",
+                     plot.background = element_rect(fill = plot_background,
                                                     colour = NA))
-  ret <- ret + theme(plot.caption = element_text(margin = margin(t = 25, r = 0, b = 0, l = 0)))
-  ret <- ret + theme(axis.title.x = element_text(margin = margin(t = 5, r = 0, b = 0, l = 0)),
-                     axis.title.y = element_text(margin = margin(t = 0, r = 5, b = 0, l = 0)))
+  ret <- ret + theme(plot.caption = element_text(
+    margin = margin(t = 25, r = 0, b = 0, l = 0)))
+  ret <- ret + theme(
+    axis.title.x = element_text(margin = margin(t = 5, r = 0, b = 0, l = 0)),
+    axis.title.y = element_text(margin = margin(t = 0, r = 5, b = 0, l = 0))
+    )
   ret
 }
